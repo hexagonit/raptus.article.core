40055ff1f710b2961cb2472717e21538a245419b
Nejc Zupan <nejc.zupan@niteoweb.com>
2011-04-14 23:07:52 +0200

    added skeletons for testing article content type and @@components BrowserView

---
+++ raptus/article/core/tests/test_article.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import unittest2 as unittest
+
+from plone.app.testing import TEST_USER_NAME, TEST_USER_ID
+
+from raptus.article.core.tests.base import RACoreIntegrationTestCase
+
+
+class TestInstall(RACoreIntegrationTestCase):
+    """Test installation of raptus.article.core into Plone."""
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        
+        # add initial test content
+        setRoles(portal, TEST_USER_ID, ['Manager'])
+        login(portal, TEST_USER_NAME)
+        portal.invokeFactory('Article', 'article')
+
+    
+def test_suite():
+    """This sets up a test suite that actually runs the tests in the class
+    above."""
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
