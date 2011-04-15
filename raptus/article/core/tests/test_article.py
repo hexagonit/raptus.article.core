1867fe3884d3041a6b608a508bc847117cb0d718
Nejc Zupan <nejc.zupan@niteoweb.com>
2011-04-15 19:14:55 +0200

    added first tests for Article content-type

--- raptus/article/core/tests/test_article.py
+++ raptus/article/core/tests/test_article.py
@@ -2,7 +2,7 @@
 
 import unittest2 as unittest
 
-from plone.app.testing import TEST_USER_NAME, TEST_USER_ID
+from plone.app.testing import TEST_USER_NAME, TEST_USER_ID, login, setRoles
 
 from raptus.article.core.tests.base import RACoreIntegrationTestCase
 
@@ -13,13 +13,43 @@ class TestInstall(RACoreIntegrationTestCase):
     def setUp(self):
         """Custom shared utility setup for tests."""
         self.portal = self.layer['portal']
-        
-        # add initial test content
-        setRoles(portal, TEST_USER_ID, ['Manager'])
-        login(portal, TEST_USER_NAME)
-        portal.invokeFactory('Article', 'article')
 
-    
+    def test_article_fields(self):
+        """Tests if Article's field values are correctly
+        stored and can be retrieved."""
+
+        # add test Article
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        login(self.portal, TEST_USER_NAME)
+        self.portal.invokeFactory('Article', 'article',
+                             title="Article Title",
+                             description="Article Description",
+                             text="<b>Article</b> Text",
+                             hideTitle=True,
+                             hideDescription=True,
+                             )
+
+        # test it's fields
+        article = self.portal.article
+        self.assertEquals(article.title, "Article Title")
+        self.assertEquals(article.description, "Article Description")
+        self.assertEquals(article.text, "<b>Article</b> Text")
+        self.assertEquals(article.hideTitle, True)
+        self.assertEquals(article.hideDescription, True)
+
+    def test_display_dropdown_disabled(self):
+        """Tests if display drop-down menu is disabled for Articles."""
+
+        # add test Article
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        login(self.portal, TEST_USER_NAME)
+        self.portal.invokeFactory('Article', 'article')
+
+        # disable display dropdown by overriding canSetDefaultPage
+        # so it always returns False
+        self.assertEquals(self.portal.article.canSetDefaultPage(), False)
+
+
 def test_suite():
     """This sets up a test suite that actually runs the tests in the class
     above."""
