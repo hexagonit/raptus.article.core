1cee0adf40439ce5f81ff061fac23434f2a547cc
Nejc Zupan <nejc.zupan@niteoweb.com>
2011-04-25 11:23:46 +0200

    added unit tests for SetDefaults() of componentsdefault.py

--- raptus/article/core/componentsdefault.py
+++ raptus/article/core/componentsdefault.py
@@ -1,9 +1,10 @@
 from zope import interface, component
 from raptus.article.core import interfaces
 
+
 def SetDefaults(object, event):
-    """ Sets the default components defined by the registered adapters providing IDefaultComponents
-    """
+    """Sets the default components defined by the registered adapters
+    providing IDefaultComponents."""
     providers = component.getAdapters((object,), interfaces.IDefaultComponents)
     for name, provider in providers:
         for comp in provider.getComponents():
