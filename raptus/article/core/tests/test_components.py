98b10c593d22c20e3e35ab63b09837630e94b896
dokai <dokai@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-04-29 06:31:49 +0000

    testing get_components() from raptus.article.core.components module
    git-svn-id: https://svn.plone.org/svn/collective/raptus.article/raptus.article.core/branches/placeful_configuration@238302 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- raptus/article/core/tests/test_components.py
+++ raptus/article/core/tests/test_components.py
@@ -2,8 +2,9 @@
 
 import unittest2 as unittest
 
-from plone.app.testing import TEST_USER_NAME, TEST_USER_ID
+from plone.app.testing import TEST_USER_NAME, TEST_USER_ID, login, setRoles
 
+from raptus.article.core.interfaces import IComponents
 from raptus.article.core.tests.base import RACoreFunctionalTestCase
 
 
@@ -14,21 +15,28 @@ class TestView(RACoreFunctionalTestCase):
         """Custom shared utility setup for tests."""
         self.portal = self.layer['portal']
 
-    def test_component_filter(self):
-        """Test filtering and sorting of components based on the registrations
-        of their viewlets.
-        """
+        # add initial test content
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        login(self.portal, TEST_USER_NAME)
+        self.portal.invokeFactory('Article', 'article')
+
+    # def test_component_filter(self):
+    #     """Test filtering and sorting of components based on the registrations
+    #     of their viewlets.
+    #     """
         #TODO
 
     def test_get_components(self):
         """Test retrieving the list of available components.
         """
-        #TODO
+        components = IComponents(self.portal.article).getComponents()
+        self.assertEquals(u'related', components[0][0])
 
-    def test_active_components(self):
-        """Test retrieving the list of active components.
-        """
-        #TODO
+    # def test_active_components(self):
+    #     """Test retrieving the list of active components.
+    #     """
+    #     #TODO
+
 
 def test_suite():
     """This sets up a test suite that actually runs the tests in the class
