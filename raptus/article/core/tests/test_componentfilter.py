c0dfbc9d7b7ee2f5c47c8843e8469e2f8ef2e3e5
Nejc Zupan <nejc.zupan@niteoweb.com>
2011-04-30 14:54:21 +0200

    moved creation of dummy ComponentFilter into a separate method

--- raptus/article/core/tests/test_componentfilter.py
+++ raptus/article/core/tests/test_componentfilter.py
@@ -14,6 +14,14 @@ class TestProvideAllInterfaces(unittest.TestCase):
     """Unit tests for logic of all edge cases in
     raptus.article.core.components.componentfilter.provide_all_interfaces()."""
 
+    def makeComponentFilter(self):
+        """Prepares an instance of ComponentFilter."""
+        from raptus.article.core.componentfilter import ComponentFilter
+        context = mock.sentinel.context
+        request = mock.sentinel.request
+        view = mock.sentinel.view
+        return ComponentFilter(context, request, view)
+
     def makeComponent(self, provided, iface):
         """Creates a mock component that is either provided or not."""
         comp = mock.Mock(spec='interface'.split())
@@ -23,27 +31,13 @@ class TestProvideAllInterfaces(unittest.TestCase):
 
     def test_no_components(self):
         """Test when there are no components."""
-        from raptus.article.core.componentfilter import ComponentFilter
-
-        # prepare instance of ComponentFilter
-        context = mock.sentinel.context
-        request = mock.sentinel.request
-        view = mock.sentinel.view
-        filter = ComponentFilter(context, request, view)
-
-        # test
-        components = filter.provide_all_interfaces([])
+        cf = self.makeComponentFilter()
+        components = cf.provide_all_interfaces([])
         self.assertEquals(0, len(components))
 
     def test_all_interfaces_provided(self):
         """Test when all components' interfaces are already provided."""
-        from raptus.article.core.componentfilter import ComponentFilter
-
-        # prepare instance of ComponentFilter
-        context = mock.sentinel.context
-        request = mock.sentinel.request
-        view = mock.sentinel.view
-        filter = ComponentFilter(context, request, view)
+        cf = self.makeComponentFilter()
 
         # prepare dummy components
         components = [
@@ -52,21 +46,14 @@ class TestProvideAllInterfaces(unittest.TestCase):
             ]
 
         # test
-        components = filter.provide_all_interfaces(components)
+        components = cf.provide_all_interfaces(components)
         self.assertEquals(0, len(components))
 
     @mock.patch('raptus.article.core.componentfilter.interface')
     def test_no_interfaces_provided(self, zope_interface):
         """Test when none of components' interfaces are already provided."""
-        from raptus.article.core.componentfilter import ComponentFilter
-
+        cf = self.makeComponentFilter()
         zope_interface.alsoProvides.return_value = True
-
-        # prepare instance of ComponentFilter
-        context = mock.sentinel.context
-        request = mock.sentinel.request
-        view = mock.sentinel.view
-        filter = ComponentFilter(context, request, view)
 
         # prepare dummy components
         components = [
@@ -75,22 +62,15 @@ class TestProvideAllInterfaces(unittest.TestCase):
             ]
 
         # test
-        interfaces = filter.provide_all_interfaces(components)
+        interfaces = cf.provide_all_interfaces(components)
         self.assertEquals(2, len(interfaces))
         self.assertEquals([i.iface() for i in interfaces], 'foo bar'.split())
 
     @mock.patch('raptus.article.core.componentfilter.interface')
     def test_some_interfaces_provided(self, zope_interface):
         """Test when some of components' interfaces are already provided."""
-        from raptus.article.core.componentfilter import ComponentFilter
-
+        cf = self.makeComponentFilter()
         zope_interface.alsoProvides.return_value = True
-
-        # prepare instance of ComponentFilter
-        context = mock.sentinel.context
-        request = mock.sentinel.request
-        view = mock.sentinel.view
-        filter = ComponentFilter(context, request, view)
 
         # prepare dummy components
         components = [
@@ -100,56 +80,22 @@ class TestProvideAllInterfaces(unittest.TestCase):
             ]
 
         # test
-        interfaces = filter.provide_all_interfaces(components)
+        interfaces = cf.provide_all_interfaces(components)
         self.assertEquals(2, len(interfaces))
-        self.assertEquals([i.iface() for i in interfaces],
-                          'foo gallery'.split())
-
-
-class TestUnprovideNotprovided(unittest.TestCase):
-    """Unit tests for logic of all edge cases in
-    raptus.article.core.components.componentfilter.unprovide_notprovided()."""
-
-    def makeInterface(self):
-        """Creates a mock interface."""
-        comp = mock.Mock(spec='interface'.split())
-        comp.interface.noLongerProvides.return_value = True
-        return comp
-
-    def test_no_interfaces(self):
-        """Test when there are no interfaces in notprovided list."""
-        from raptus.article.core.componentfilter import ComponentFilter
-
-        # prepare instance of ComponentFilter
-        context = mock.sentinel.context
-        request = mock.sentinel.request
-        view = mock.sentinel.view
-        filter = ComponentFilter(context, request, view)
-
-        # test
-        filter.unprovide_notprovided([])
-
-    @mock.patch('raptus.article.core.componentfilter.interface')
-    def test_unprovide_interfaces(self, zope_interface):
-        """Test unproviding interfaces notprovided list."""
-        from raptus.article.core.componentfilter import ComponentFilter
-
-        # prepare instance of ComponentFilter
-        context = mock.sentinel.context
-        request = mock.sentinel.request
-        view = mock.sentinel.view
-        filter = ComponentFilter(context, request, view)
-
-        zope_interface.noLongerProvides.return_value = True
-
-        # test
-        filter.unprovide_notprovided("foo bar".split())
-        self.assertEquals(zope_interface.noLongerProvides.call_count, 2)
+        self.assertEquals([i.iface() for i in interfaces], 'foo gallery'.split())
 
 
 class TestGetOrderedViewlets(unittest.TestCase):
     """Unit tests for logic of all edge cases in
     raptus.article.core.components.componentfilter.get_ordered_viewlets()."""
+
+    def makeComponentFilter(self):
+        """Prepares an instance of ComponentFilter."""
+        from raptus.article.core.componentfilter import ComponentFilter
+        context = mock.sentinel.context
+        request = mock.sentinel.request
+        view = mock.sentinel.view
+        return ComponentFilter(context, request, view)
 
     def makeNamelessViewlet(self, name):
         """Creates a mock viewlet withouth a __name__."""
@@ -170,59 +116,43 @@ class TestGetOrderedViewlets(unittest.TestCase):
     def test_no_viewlet_managers(self):
         """Test retriving an ordered list of viewlets when no viewlet managers
         are specified."""
-        from raptus.article.core.componentfilter import ComponentFilter
-        from raptus.article.core import componentfilter
 
-        # prepare instance of ComponentFilter
-        context = mock.sentinel.context
-        request = mock.sentinel.request
-        view = mock.sentinel.view
-        filter = ComponentFilter(context, request, view)
+        cf = self.makeComponentFilter()
 
         # override ORDERED_VIEWLET_MANAGERS to be an empty list
+        from raptus.article.core import componentfilter
         componentfilter.ORDERED_VIEWLET_MANAGERS = []
 
         # test
-        order = filter.get_ordered_viewlets()
+        order = cf.get_ordered_viewlets()
         self.assertEquals(len(order), 0)
 
     @mock.patch('raptus.article.core.componentfilter.ComponentFilter.get_viewlet_manager')
     def test_invalid_manager(self, get_viewlet_manager):
         """Test retriving a list of ordered viewlets when a viewlet manager
         is invalid and cannot be found."""
-        from zope.component.interfaces import ComponentLookupError
-        from raptus.article.core.componentfilter import ComponentFilter
 
-        # prepare instance of ComponentFilter
-        context = mock.sentinel.context
-        request = mock.sentinel.request
-        view = mock.sentinel.view
-        filter = ComponentFilter(context, request, view)
-
+        cf = self.makeComponentFilter()
+        
         # self.get_viewlet_manager(name, iface) throws the ComponentLookupError
+        from zope.component.interfaces import ComponentLookupError
         get_viewlet_manager.return_value = None
         get_viewlet_manager.side_effect = ComponentLookupError('foo')
 
         # test that get_ordered_viewlets does not crash when it receives
         # the ComponentLookupError
-        order = filter.get_ordered_viewlets()
+        order = cf.get_ordered_viewlets()
         self.assertEquals(len(order), 0)
 
-    @mock.patch(
-    'raptus.article.core.componentfilter.ComponentFilter.get_viewlet_manager')
+    @mock.patch('raptus.article.core.componentfilter.ComponentFilter.get_viewlet_manager')
     def test_viewlet_without_name(self, get_viewlet_manager):
         """Test retriving a list of ordered viewlets when a viewlet does not
         have a __name__."""
-        from raptus.article.core.componentfilter import ComponentFilter
-        from raptus.article.core import componentfilter
 
-        # prepare instance of ComponentFilter
-        context = mock.sentinel.context
-        request = mock.sentinel.request
-        view = mock.sentinel.view
-        filter = ComponentFilter(context, request, view)
+        cf = self.makeComponentFilter()
 
         # override ORDERED_VIEWLET_MANAGERS to only have one item
+        from raptus.article.core import componentfilter
         componentfilter.ORDERED_VIEWLET_MANAGERS = [('foo', 'bar')]
 
         # create mocked viewlet and viewlet manager
@@ -234,7 +164,7 @@ class TestGetOrderedViewlets(unittest.TestCase):
 
         # test that get_ordered_viewlets does not crash when it receives
         # the ComponentLookupError
-        order = filter.get_ordered_viewlets()
+        order = cf.get_ordered_viewlets()
         self.assertEquals(len(order), 0)
 
 
@@ -251,19 +181,20 @@ class TestGetOrderdViewletsIntegration(RACoreIntegrationTestCase):
         login(self.portal, TEST_USER_NAME)
         self.portal.invokeFactory('Article', 'article')
 
-    def test_get_ordered_viewlets(self):
-        """Test retriving a list of viewlets ordered by their viewlet
-        managers."""
+    def makeComponentFilter(self):
+        """Prepares an instance of ComponentFilter."""
         from raptus.article.core.componentfilter import ComponentFilter
-
-        # prepare instance of ComponentFilter
         context = self.portal.article
         request = self.layer['request']
         view = self.portal.restrictedTraverse('article')
-        filter = ComponentFilter(context, request, view)
+        return ComponentFilter(context, request, view)
 
-        # test
-        order = filter.get_ordered_viewlets()
+    def test_get_ordered_viewlets(self):
+        """Test retriving a list of viewlets ordered by their viewlet
+        managers."""
+        cf = self.makeComponentFilter()
+        
+        order = cf.get_ordered_viewlets()
         self.assertEquals(len(order), 29)
         self.assertEquals(order[0], u'plone.htmlhead.dublincore')
         self.assertEquals(order[len(order) - 1], u'plone.analytics')
@@ -282,19 +213,20 @@ class TestGetViewletManagerIntegration(RACoreIntegrationTestCase):
         login(self.portal, TEST_USER_NAME)
         self.portal.invokeFactory('Article', 'article')
 
-    def test_retrieve_viewlet_manager(self):
-        """Test retriving a viewlet manager."""
+    def makeComponentFilter(self):
+        """Prepares an instance of ComponentFilter."""
         from raptus.article.core.componentfilter import ComponentFilter
-        from plone.app.layout.viewlets.interfaces import IHtmlHead
-
-        # prepare instance of ComponentFilter
         context = self.portal.article
         request = self.layer['request']
         view = self.portal.restrictedTraverse('article')
-        filter = ComponentFilter(context, request, view)
+        return ComponentFilter(context, request, view)
 
-        # test
-        manager = filter.get_viewlet_manager('plone.htmlhead', IHtmlHead)
+    def test_retrieve_viewlet_manager(self):
+        """Test retriving a viewlet manager."""
+        from plone.app.layout.viewlets.interfaces import IHtmlHead
+        cf = self.makeComponentFilter()
+        
+        manager = cf.get_viewlet_manager('plone.htmlhead', IHtmlHead)
         self.assertTrue(manager)
         self.assertTrue(IHtmlHead.providedBy(manager))
 
@@ -302,6 +234,14 @@ class TestGetViewletManagerIntegration(RACoreIntegrationTestCase):
 class TestSortComponents(unittest.TestCase):
     """Unit tests for logic of all edge cases in
     raptus.article.core.components.componentfilter.sort_components()."""
+
+    def makeComponentFilter(self):
+        """Prepares an instance of ComponentFilter."""
+        from raptus.article.core.componentfilter import ComponentFilter
+        context = mock.sentinel.context
+        request = mock.sentinel.request
+        view = mock.sentinel.view
+        return ComponentFilter(context, request, view)
 
     def makeComponent(self, viewlet):
         """Creates a mock component."""
@@ -313,28 +253,14 @@ class TestSortComponents(unittest.TestCase):
 
     def test_no_components(self):
         """Test when there are no components."""
-        from raptus.article.core.componentfilter import ComponentFilter
-
-        # prepare instance of ComponentFilter
-        context = mock.sentinel.context
-        request = mock.sentinel.request
-        view = mock.sentinel.view
-        filter = ComponentFilter(context, request, view)
-
-        # test
-        components = filter.sort_components([], [])
+        cf = self.makeComponentFilter()
+        components = cf.sort_components([], [])
         self.assertEquals(0, len(components))
 
     def test_sort_components(self):
         """Test sorting components."""
-        from raptus.article.core.componentfilter import ComponentFilter
-
-        # prepare instance of ComponentFilter
-        context = mock.sentinel.context
-        request = mock.sentinel.request
-        view = mock.sentinel.view
-        filter = ComponentFilter(context, request, view)
-
+        cf = self.makeComponentFilter()
+        
         components = [
             ('foo', self.makeComponent('foo')),
             ('bar', self.makeComponent('bar')),
@@ -347,8 +273,7 @@ class TestSortComponents(unittest.TestCase):
             'foo',
         )
 
-        # test
-        sorted_comps = filter.sort_components(components, order)
+        sorted_comps = cf.sort_components(components, order)
         self.assertEquals(3, len(sorted_comps))
         self.assertEquals([comp[0] for comp in sorted_comps],
                           'bar gallery foo'.split())
@@ -358,18 +283,18 @@ class TestFilter(unittest.TestCase):
     """Unit tests for logic of all edge cases in
     raptus.article.core.components.componentfilter.filter()."""
 
-    def test_no_components(self):
-        """Test when there are no components."""
+    def makeComponentFilter(self):
+        """Prepares an instance of ComponentFilter."""
         from raptus.article.core.componentfilter import ComponentFilter
-
-        # prepare instance of ComponentFilter
         context = mock.sentinel.context
         request = mock.sentinel.request
         view = mock.sentinel.view
-        filter = ComponentFilter(context, request, view)
+        return ComponentFilter(context, request, view)
 
-        # test
-        components = filter.filter([])
+    def test_no_components(self):
+        """Test when there are no components."""
+        cf = self.makeComponentFilter()
+        components = cf.filter([])
         self.assertEquals(0, len(components))
 
 
@@ -385,23 +310,26 @@ class TestFilterIntegration(RACoreIntegrationTestCase):
         login(self.portal, TEST_USER_NAME)
         self.portal.invokeFactory('Article', 'article')
 
-    def test_default_component(self):
-        """Test sorting components available by default."""
+    def makeComponentFilter(self):
+        """Prepares an instance of ComponentFilter."""
         from raptus.article.core.componentfilter import ComponentFilter
-        from raptus.article.core.interfaces import IComponents
-
-        # prepare instance of ComponentFilter
         context = self.portal.article
         request = self.layer['request']
         view = self.portal.restrictedTraverse('article')
-        filter = ComponentFilter(context, request, view)
+        return ComponentFilter(context, request, view)
 
+    def test_default_component(self):
+        """Test sorting components available by default."""
+
+        cf = self.makeComponentFilter()
+        
         # get a list of RA components to pass into filtering
+        from raptus.article.core.interfaces import IComponents
         components = IComponents(self.portal.article).getComponents()
         self.assertEquals(len(components), 1)
 
         # test
-        sorted_components = filter.filter(components)
+        sorted_components = cf.filter(components)
         self.assertEquals(len(sorted_components), 1)
 
         name, comp = sorted_components[0]
