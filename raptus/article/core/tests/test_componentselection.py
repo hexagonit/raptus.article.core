5d5c4878639cb46428da143d2c1066f3cfbce66b
Nejc Zupan <nejc.zupan@niteoweb.com>
2011-05-11 11:46:05 +0200

    added integration test for ComponentSelectionDefault

--- raptus/article/core/tests/test_componentselection.py
+++ raptus/article/core/tests/test_componentselection.py
@@ -46,6 +46,28 @@ class TestComponentSelectionWidgetIntegration(RACoreIntegrationTestCase):
         widget = self.makeComponentSelectionWidget()
         self.assertEquals('invisible', widget.isVisible(self.portal))
 
+class TestComponentSelectionDefaultIntegration(RACoreIntegrationTestCase):
+    """Integration tests for ComponentSelectionVocabulary."""
+
+    def makeComponentSelectionDefault(self):
+        """Prepares an instance of ComponentSelection."""
+        from raptus.article.core.componentselection import ComponentSelectionDefault
+        return ComponentSelectionDefault(self.portal.article)
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+
+        # add initial test content
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        login(self.portal, TEST_USER_NAME)
+        self.portal.invokeFactory('Article', 'article')
+
+    def test_call(self):
+        """Test __call__() of ComponentSelectionWidget."""
+        default = self.makeComponentSelectionDefault()
+        self.assertEquals([], default())
+
 
 def test_suite():
     """This sets up a test suite that actually runs the tests in the class
