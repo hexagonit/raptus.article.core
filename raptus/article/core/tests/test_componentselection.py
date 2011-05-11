193e4048a055124a9f9ddeff49c575f6a17fd3e3
Nejc Zupan <nejc.zupan@niteoweb.com>
2011-05-11 11:22:06 +0200

    added integration test for ComponentSelectionWidget

---
+++ raptus/article/core/tests/test_componentselection.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import TEST_USER_NAME
+from plone.app.testing import login
+from plone.app.testing import setRoles
+
+from raptus.article.core.tests.base import RACoreIntegrationTestCase
+
+import unittest2 as unittest
+
+
+class TestComponentSelectionWidgetIntegration(RACoreIntegrationTestCase):
+    """Integration tests for ComponentSelectionVocabulary."""
+
+    def makeComponentSelectionWidget(self):
+        """Prepares an instance of ComponentSelection."""
+        from raptus.article.core.componentselection import ComponentSelectionWidget
+        return ComponentSelectionWidget()
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+
+        # add initial test content
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        login(self.portal, TEST_USER_NAME)
+        self.portal.invokeFactory('Article', 'article')
+
+    def test_is_visible(self):
+        """Test is_visible() of ComponentSelectionWidget on a child
+        of Article."""
+
+        # create a subcontent in Article
+        self.portal.article.invokeFactory('Folder', 'subfolder')
+        subfolder = self.portal.article.subfolder
+
+        # check if subfolder is contained in an Article
+        widget = self.makeComponentSelectionWidget()
+        self.assertEquals('visible', widget.isVisible(subfolder))
+
+    def test_not_visible(self):
+        """Test is_visible() of ComponentSelectionWidget on a non-Article
+        child.
+        """
+        widget = self.makeComponentSelectionWidget()
+        self.assertEquals('invisible', widget.isVisible(self.portal))
+
+
+def test_suite():
+    """This sets up a test suite that actually runs the tests in the class
+    above."""
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
