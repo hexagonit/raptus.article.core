bfcd7a33dc3bc623041aee69d7d985b5af135097
Nejc Zupan <nejc.zupan@niteoweb.com>
2011-05-11 15:16:13 +0200

    added integration tests for _save()

--- raptus/article/core/tests/test_browser_components.py
+++ raptus/article/core/tests/test_browser_components.py
@@ -13,14 +13,56 @@ import unittest2 as unittest
 class TestComponentsIntegration(RACoreIntegrationTestCase):
     """Test components() method of raptus.article.core.browser.components."""
 
+    def get_active_components(self):
+        """Get components that are activated for Article."""
+        from raptus.article.core.interfaces import IComponents
+        return IComponents(self.portal.article).activeComponents()
+
     def setUp(self):
         """Custom shared utility setup for tests."""
         self.portal = self.layer['portal']
+        self.request = self.layer['request']
 
         # add initial test content
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
         login(self.portal, TEST_USER_NAME)
         self.portal.invokeFactory('Article', 'article')
+
+    def test_activate_component(self):
+        """Test that _save() activates components set in request."""
+        # check that no components are active by default
+        self.assertEquals(0, len(self.get_active_components()))
+
+        # call _save() to activate related component
+        self.request.form['form.components'] = u'related'
+        view = self.portal.article.restrictedTraverse('@@components')
+        result = view._save()
+        self.assertEquals(True, result)
+
+        # check that r.a.related component is now active
+        active_components = self.get_active_components()
+        self.assertEquals(1, len(active_components))
+        name, comp = active_components[0]
+        self.assertEquals(name, 'related')
+
+    def test_deactivate_component(self):
+        """Test that _save() deactivates components set in request."""
+        # check that no components are active by default
+        self.assertEquals(0, len(self.get_active_components()))
+
+        # activate the default component
+        from raptus.article.core.browser.related import IRelated
+        alsoProvides(self.portal.article, IRelated)
+        self.assertEquals(1, len(self.get_active_components()))
+
+        # call _save() to deactivate related component
+        view = self.portal.article.restrictedTraverse('@@components')
+        result = view._save()
+        self.assertEquals(True, result)
+
+        # check that r.a.related component is deactivated
+        active_components = self.get_active_components()
+        self.assertEquals(0, len(active_components))
 
     def test_default_component(self):
         """Test component 'related' that is available by default."""
