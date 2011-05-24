d2762c19a6ffe7460105163b3582e1f4476d0a0a
Nejc Zupan <nejc.zupan@gmail.com>
2011-05-24 16:11:20 +0300

    renamed README.txt into README.rst so github will display it more nicely

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ install_requires = [
 setup(name='raptus.article.core',
       version=version,
       description="Provides a configurable article content type (replaces the default Page content type).",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "MANUAL.txt")).read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
